21c74bfc0fc2ee6ced54977b1565240e922778d2
UNKNOWDEV <unknowsodev@gmail.com>
2025-12-17 08:01:21 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,15 +2,15 @@ from flask import Flask
 from threading import Thread
 import logging
 
-# ปิด Log ขยะของ Flask
+# ปิด Log ขยะ
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>🤖 SYSTEM STATUS: ONLINE</h1><p>Bot is running...</p>"
+    return "<h1>🤖 Bot is Online!</h1>"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
